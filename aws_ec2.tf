5fe3faee896acd9d01785d507872382cdafb7acc
Songwut Cotcharat <songwut@this.fish>
2020-10-09 17:42:04 +0700

    added providers

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,10 +1,11 @@
+#define a provider
 provider "aws" {
-  region     = "ap-southeast-1"
-  access_key = "key"
+  region     = "ap-southeast-1" 
+  access_key = "key" 
   secret_key = "key"
 }
-
+#resource on AWS instance
 resource "aws_instance" "makok1" {
-  ami           = "ami-015a6758451df3cb9"
-  instance_type = "t2.micro"
+  ami           = "ami-015a6758451df3cb9"  #aws images
+  instance_type = "t2.micro"  
 }
---
+++ md file/desired - current state.md
@@ -0,0 +1,11 @@
+
+### Desired and Current State
+
+i) Desired State: EC2 = instance_type = "t2.micro"
+
+ii) Current State: EC2 = "instance_state": "stopped" && "instance_type" = "t2.nano"
+
+Desired State == Current State
+
+earlier: default
+now: MK1
