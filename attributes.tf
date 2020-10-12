3ae81c9713e08a7441f282187e305e9476d54bf2
Songwut Cotcharat <songwut@this.fish>
2020-10-12 11:31:12 +0700

    added rsa-key

--- attributes.tf
+++ attributes.tf
@@ -16,3 +16,4 @@ output "mys3bucket" {
 }
 */
 
+
--- aws_ec2.tf
+++ aws_ec2.tf
@@ -2,4 +2,9 @@
 resource "aws_instance" "makok1" {
   ami           = "ami-015a6758451df3cb9"  #aws images
   instance_type = "t2.micro"  
+  key_name      = "mk1"
+}
+resource "aws_key_pair" "deployer" {
+  key_name   = "mk1"
+  public_key = "your public key"
 }
